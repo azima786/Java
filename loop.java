586593463bf0d5aa6460c2023e5019f48d2c0c28
Azima Keshwani <azima786@gmail.com>
2022-08-29 15:55:15 -0500

    update

--- loop.java
+++ loop.java
@@ -1,13 +1,40 @@
 public class loop {
 
 	public static void main(String[] args) {
-		for (int i = 3; i <= 0; i--) {
-			for (int j = 0; j <= i; j++) {
+		//for (int i = 3; i <= 0; i--) {
+		//	for (int j = 0; j <= i; j++) {
 
-			}
-			
-			System.out.println("*");
-		}
+		//	}
 
+		//	System.out.println("*");
+		//}
+		//	int sum = 0;
+		//	int n = 3;
+		//	//even numbers
+		//	for (int i = 0; i <= n; i++) {
+
+		//			sum = sum+ 2*i;
+
+		//	}
+		//System.out.println(sum);
+		int n = 8;
+		int sum = 0;
+			for (int i = 10; i >= 1; i--) {
+				sum += n*i;
+		System.out.printf("%d x %d = %d\n", n, i, n*i);
+System.out.println(sum);
+				}
+
+	//	int n = 5;
+	////	int i = 1;
+	//	int factorial = 1;
+	//	//while (i <= n) {
+	//	//	factorial *= i;
+	//	//	i++;
+	//	//}
+	//	for (int i = 1; i <= n; i++) {
+	//		factorial *= i;
+	//	}
+	//	System.out.println(factorial);
 	}
 }
