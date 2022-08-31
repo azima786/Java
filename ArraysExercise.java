1844e7ce20d381c3a824b6e7b8218ed9879abbc2
Azima Keshwani <azima786@gmail.com>
2022-08-31 09:09:46 -0500

    update

--- ArraysExercise.java
+++ ArraysExercise.java
@@ -34,19 +34,37 @@ public class ArraysExercise {
 
 		// System.out.println(sum/marks.length);
 
-		int[][] mat1 = { { 1, 2, 3 }, { 4, 5, 6 } };
-		int[][] mat2 = { { 2, 6, 13 }, { 3, 7, 1 } };
-		int[][] result = { { 0, 0, 0 }, { 0, 0, 0 } };
-	
-		for (int i = 0; i < mat1.length; i++) {
-			for (int j = 0; j < mat1[i].length; j++) {
-				//System.out.format("i=%d and j=%d\n", i, j);
-			
-				result[i][j] = mat1[i][j] + mat2[i][j];
-				System.out.print(result[i][j] + " ");
-			}
-		System.out.println("");
-		}
-	
+		//int[][] mat1 = { { 1, 2, 3 }, { 4, 5, 6 } };
+		//int[][] mat2 = { { 2, 6, 13 }, { 3, 7, 1 } };
+		//int[][] result = { { 0, 0, 0 }, { 0, 0, 0 } };
+
+		//for (int i = 0; i < mat1.length; i++) {
+		//	for (int j = 0; j < mat1[i].length; j++) {
+		//		//System.out.format("i=%d and j=%d\n", i, j);
+
+		//		result[i][j] = mat1[i][j] + mat2[i][j];
+		//		System.out.print(result[i][j] + " ");
+		//	}
+		//System.out.println("");
+		//}
+
+		//Reverse an Array
+		//		int[] arr = { 1, 8, 3, 4, 5, 6 };
+
+		//		int l = arr.length;
+		//		int n = Math.floorDiv(l, 2);
+
+		//System.out.println(n);
+		//int temp;
+		//for (int i = 0; i < n; i++) {
+		//	temp = arr[i];
+		//	arr[i] = arr[l - i - 1];
+		//	arr[l - i - 1] = temp;
+		//}
+		//for (int element : arr) {
+		//System.out.print(element);
+		//}
+
+
 	}
 }
--- MultiDimensionalArrays.java
+++ MultiDimensionalArrays.java
@@ -2,9 +2,9 @@ public class MultiDimensionalArrays {
 	public static void main(String[] args) {
 		int flats[][] = { { 101, 102, 103 }, { 201, 202, 203 } };
 		for (int i = 0; i < flats.length; i++) {
-			for (int j = 0; j < flats[i].length; i++) {
-			System.out.println(flats[i][j]);
+			for (int j = 0; j < flats[i].length; j++) {
 			
+			System.out.println(flats[i][j]);
 			}
 		}
 
